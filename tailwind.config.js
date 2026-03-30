a8d3a8598299dc5f7bcc23953cd9d6ccdd77f582
jefreegutierrezz <jefreegoficial@gmail.com>
2026-03-30 18:09:49 -0500

    feat: estructura inicial bento grid y configuración de archivos

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Aquí le decimos a Tailwind que vigile tu HTML y tus archivos JS en /src
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#6366f1', // Nuestro Indigo moderno
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
